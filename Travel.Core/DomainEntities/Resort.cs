a495d609673fc6a0fcf487236fa171fdfc2b011d
MaciejDudzinski <Maciej.Dudzinski@lowcosttravelgroup.com>
2016-07-07 17:06:56 +0100

    commit json files

---
+++ Travel.Core/DomainEntities/Resort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.Core.DomainEntities
+{
+    public class Resort
+    {
+        public int ID { get; set; }
+    }
+}
