f57b01c4a7e8185e57f337f6dde1879b5694cbfb
Jas05 <Jasmeet05@gmail.com>
2016-07-07 18:10:47 +0100

    Activity stuff added

---
+++ Travel.Web/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Travel.Web.Models;
+
+namespace Travel.Web.ViewModels
+{
+    public class ActivityViewModel
+    {
+        public List<ActivityModel> Activities { get; set; }
+    }
+}
