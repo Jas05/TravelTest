dce9624814d8aa9ff9ea299d878acba5ede0d366
Jas05 <Jasmeet05@gmail.com>
2016-07-07 20:19:52 +0100

    Itinerary added

---
+++ Travel.Web/Controllers/ItineraryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Travel.Core.DAL;
+using Travel.Web.ViewModels;
+
+namespace Travel.Web.Controllers
+{
+    public class ItineraryController : Controller
+    {
+        // GET: Itinerary
+        public ActionResult Index(string bookingRef)
+        {
+            var viewModel = new ItineraryViewModel();
+            var datamanager = new DataManager();
+            // var bookingDetails = datamanager.GetBookingDetails(bookingRef);
+            //var flights = datamanager.GetDestinationDetails(bookingDetails.De)
+            return View();
+        }
+    }
+}
---
+++ Travel.Web/ViewModels/ItineraryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Travel.Web.ViewModels
+{
+    public class ItineraryViewModel
+    {
+        public string DestinationName { get; set; }
+
+        public string ArrivalName { get; set; }
+
+        public string OutboundDepartureDatetime { get; set; }
+
+        public string OutboundArrivalDateTime { get; set; }
+
+        public string HotelName { get; set; }
+
+        public string Duration { get; set; }
+
+        public string BoardBasis { get; set; }
+
+        public string InboundDepartureTime { get; set; }
+
+        public string InboundArrivalTime { get; set; }
+
+    }
+}
