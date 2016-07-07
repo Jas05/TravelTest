eb6e8e5becb19f22050631047bfd9de66b73228e
keshavmhamal <KeshavMhamal@LowCostBeds.Com>
2016-07-07 20:30:27 +0100

    Added code

--- Travel.Core/DomainEntities/Flight.cs
+++ Travel.Core/DomainEntities/Flight.cs
@@ -14,5 +14,17 @@ namespace Travel.Core.DomainEntities
     public class Flight
     {
         public string ID { get; set; }
+        public string InboundFlightDate { get; set; }
+        public string InboundFlightTime { get; set; }
+        public string InboundFlightDuration { get; set; }
+        public string InboundFlightOperator { get; set; }
+        public string InboundFlightCode { get; set; }
+        public string OutboundFlightDate { get; set; }
+        public string OutboundFlightTime { get; set; }
+        public string OutboundFlightDuration { get; set; }
+        public string OutboundFlightOperator { get; set; }
+        public string OutboundFlightCode { get; set; }
+        public string FlightFrom { get; set; }
+        public string FlightTo { get; set; }
     }
 }
