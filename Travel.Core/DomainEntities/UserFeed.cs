0085582e8196ec2de96f0ff9bdd9e4e859ac9b52
MaciejDudzinski <Maciej.Dudzinski@lowcosttravelgroup.com>
2016-07-07 17:26:57 +0100

    updating json files

---
+++ Travel.Core/DomainEntities/UserFeed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.Core.DomainEntities
+{
+    public class UserFeed
+    {
+        public int ID { get; set; }
+        public int DestinationID { get; set; }
+    }
+}
