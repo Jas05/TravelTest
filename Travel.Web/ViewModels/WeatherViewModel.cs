d21de116ffcf933b15bd09a12b5b93304a23f847
keshavmhamal <KeshavMhamal@LowCostBeds.Com>
2016-07-08 09:44:13 +0100

    Added Code

---
+++ Travel.Web/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Travel.Core.DomainEntities;
+
+namespace Travel.Web.ViewModels
+{
+    public class WeatherViewModel
+    {
+        public WeatherInformation WeatherInformation { get; set; }
+    }
+}
