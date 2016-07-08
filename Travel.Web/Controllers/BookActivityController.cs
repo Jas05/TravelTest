d243529a1042b261e6db2011037c0fffcd392c7d
keshavmhamal <KeshavMhamal@LowCostBeds.Com>
2016-07-08 11:24:49 +0100

    Added Code

---
+++ Travel.Web/Controllers/BookActivityController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Travel.Core.DomainEntities;
+
+namespace Travel.Web.Controllers
+{
+    public class BookActivityController : Controller
+    {
+        // GET: BookActivity
+        public ActionResult Index(Activity2 Activity)
+        {
+            return View(Activity);
+        }
+    }
+}
